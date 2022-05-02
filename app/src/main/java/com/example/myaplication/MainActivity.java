a11dae13690035926f85e44d04c1fa638be3f89b
Yan NOW <tumakaev.jan@yandex.ru>
2022-05-02 18:12:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myaplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textCounter1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maib_linear);
+
+        textCounter1 = findViewById(R.id.textView);
+    }
+
+    public void 
+}
